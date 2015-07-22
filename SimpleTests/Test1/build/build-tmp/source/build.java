a766ebf25d816d76f0e32ef5d31ed4dd1302cbd8
Jason B <bjason@gmail.com>
2015-07-22 01:05:54 -0700

    Build.

---
+++ SimpleTests/Test1/build/build-tmp/source/build.java
@@ -0,0 +1,37 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class build extends PApplet {
+
+public void setup(){
+	size(600, 600);
+
+
+	for(int i = 0; i < 10; i++){
+		rect(1,50*i,50,50);
+	
+	}
+	
+
+
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "build" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ SimpleTests/Test1/build/build.pde
@@ -0,0 +1,14 @@
+void setup(){
+	size(600, 600);
+
+
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 10; j++){
+			rect(10*j,50*i,50,50);
+		}
+	
+	}
+	
+
+
+}
